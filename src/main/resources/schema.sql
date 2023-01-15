36e8ad94220837441a29d4ea739b74b87b728b5c
Marius Grafu <grafumarius@gmail.com>
2023-01-15 15:21:56 +0200

    prettify schema sql + postman collection

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,318 +1,98 @@
-CREATE
-DATABASE IF NOT EXISTS personalAgendaApp;
+CREATE DATABASE IF NOT EXISTS personalagendaapp;
 
 CREATE TABLE IF NOT EXISTS users
 (
-    id
-    INT
-    NOT
-    NULL
-    AUTO_INCREMENT,
-    name
-    VARCHAR
-(
-    100
-) NOT NULL,
-    email VARCHAR
-(
-    100
-) NOT NULL,
-    password VARCHAR
-(
-    100
-) NOT NULL,
-    date DATETIME NOT NULL,
-    PRIMARY KEY
-(
-    id
-)
+    id       INT NOT NULL auto_increment,
+    name     VARCHAR (100) NOT NULL,
+    email    VARCHAR (100) NOT NULL,
+    password VARCHAR (100) NOT NULL,
+    date     DATETIME NOT NULL,
+    PRIMARY KEY ( id )
     );
 
 CREATE TABLE IF NOT EXISTS categories
 (
-    id
-    INT
-    NOT
-    NULL
-    AUTO_INCREMENT,
-    name
-    VARCHAR
-(
-    100
-) NOT NULL,
-    PRIMARY KEY
-(
-    id
-)
+    id   INT NOT NULL auto_increment,
+    name VARCHAR (100) NOT NULL,
+    PRIMARY KEY ( id )
     );
 
 CREATE TABLE IF NOT EXISTS events
 (
-    id
-    INT
-    NOT
-    NULL
-    AUTO_INCREMENT,
-    name
-    VARCHAR
-(
-    100
-) NOT NULL,
-    date DATETIME NOT NULL,
-    location VARCHAR
-(
-    100
-) NOT NULL,
-    authorId INT NOT NULL,
-    categoryId INT NULL,
-    PRIMARY KEY
-(
-    id
-),
-    FOREIGN KEY
-(
-    categoryId
-) REFERENCES categories
-(
-    id
-),
-    FOREIGN KEY
-(
-    authorId
-) REFERENCES users
-(
-    id
-)
+    id         INT NOT NULL auto_increment,
+    name       VARCHAR (100) NOT NULL,
+    date       DATETIME NOT NULL,
+    location   VARCHAR (100) NOT NULL,
+    authorid   INT NOT NULL,
+    categoryid INT NULL,
+    PRIMARY KEY ( id ),
+    FOREIGN KEY ( categoryid ) REFERENCES categories ( id ),
+    FOREIGN KEY ( authorid ) REFERENCES users ( id )
     );
-
 
 CREATE TABLE IF NOT EXISTS attendees
 (
-    eventId
-    INT
-    NOT
-    NULL,
-    userId
-    INT
-    NOT
-    NULL,
-    PRIMARY
-    KEY
-(
-    eventId,
-    userId
-),
-    FOREIGN KEY
-(
-    eventId
-) REFERENCES events
-(
-    id
-),
-    FOREIGN KEY
-(
-    userId
-) REFERENCES users
-(
-    id
-)
+    eventid INT NOT NULL,
+    userid  INT NOT NULL,
+    PRIMARY KEY ( eventid, userid ),
+    FOREIGN KEY ( eventid ) REFERENCES events ( id ),
+    FOREIGN KEY ( userid ) REFERENCES users ( id )
     );
 
 CREATE TABLE IF NOT EXISTS tasks
 (
-    id
-    INT
-    NOT
-    NULL
-    AUTO_INCREMENT,
-    name
-    VARCHAR
-(
-    100
-) NOT NULL,
-    startDate DATETIME NOT NULL,
-    endDate DATETIME NOT NULL,
-    description VARCHAR
-(
-    500
-) NOT NULL,
-    isDone BIT NOT NULL,
-    authorId INT NOT NULL,
-    categoryId INT NULL,
-    PRIMARY KEY
-(
-    id
-),
-    FOREIGN KEY
-(
-    categoryId
-) REFERENCES categories
-(
-    id
-),
-    FOREIGN KEY
-(
-    authorId
-) REFERENCES users
-(
-    id
-)
+    id          INT NOT NULL auto_increment,
+    name        VARCHAR (100) NOT NULL,
+    startdate   DATETIME NOT NULL,
+    enddate     DATETIME NOT NULL,
+    description VARCHAR (500) NOT NULL,
+    isdone      BIT NOT NULL,
+    authorid    INT NOT NULL,
+    categoryid  INT NULL,
+    PRIMARY KEY ( id ),
+    FOREIGN KEY ( categoryid ) REFERENCES categories ( id ),
+    FOREIGN KEY ( authorid ) REFERENCES users ( id )
     );
 
 CREATE TABLE IF NOT EXISTS collaborators
 (
-    taskId
-    INT
-    NOT
-    NULL,
-    userId
-    INT
-    NOT
-    NULL,
-    PRIMARY
-    KEY
-(
-    taskId,
-    userId
-),
-    FOREIGN KEY
-(
-    taskId
-) REFERENCES tasks
-(
-    id
-),
-    FOREIGN KEY
-(
-    userId
-) REFERENCES users
-(
-    id
-)
+    taskid INT NOT NULL,
+    userid INT NOT NULL,
+    PRIMARY KEY ( taskid, userid ),
+    FOREIGN KEY ( taskid ) REFERENCES tasks ( id ),
+    FOREIGN KEY ( userid ) REFERENCES users ( id )
     );
 
-CREATE TABLE IF NOT EXISTS eventInvites
+CREATE TABLE IF NOT EXISTS eventinvites
 (
-    id
-    INT
-    NOT
-    NULL
-    AUTO_INCREMENT,
-    eventId
-    INT
-    NOT
-    NULL,
-    userId
-    INT
-    NOT
-    NULL,
-    sentDate
-    DATETIME
-    NOT
-    NULL,
-    PRIMARY
-    KEY
-(
-    id
-),
-    FOREIGN KEY
-(
-    eventId
-) REFERENCES events
-(
-    id
-),
-    FOREIGN KEY
-(
-    userId
-) REFERENCES users
-(
-    id
-)
+    id       INT NOT NULL auto_increment,
+    eventid  INT NOT NULL,
+    userid   INT NOT NULL,
+    sentdate DATETIME NOT NULL,
+    PRIMARY KEY ( id ),
+    FOREIGN KEY ( eventid ) REFERENCES events ( id ),
+    FOREIGN KEY ( userid ) REFERENCES users ( id )
     );
 
-
-CREATE TABLE IF NOT EXISTS taskInvites
+CREATE TABLE IF NOT EXISTS taskinvites
 (
-    id
-    INT
-    NOT
-    NULL
-    AUTO_INCREMENT,
-    taskId
-    INT
-    NOT
-    NULL,
-    userId
-    INT
-    NOT
-    NULL,
-    sentDate
-    DATETIME
-    NOT
-    NULL,
-    PRIMARY
-    KEY
-(
-    id
-),
-    FOREIGN KEY
-(
-    taskId
-) REFERENCES tasks
-(
-    id
-),
-    FOREIGN KEY
-(
-    userId
-) REFERENCES users
-(
-    id
-)
+    id       INT NOT NULL auto_increment,
+    taskid   INT NOT NULL,
+    userid   INT NOT NULL,
+    sentdate DATETIME NOT NULL,
+    PRIMARY KEY ( id ),
+    FOREIGN KEY ( taskid ) REFERENCES tasks ( id ),
+    FOREIGN KEY ( userid ) REFERENCES users ( id )
     );
-
 
 CREATE TABLE IF NOT EXISTS notes
 (
-    id
-    INT
-    NOT
-    NULL
-    AUTO_INCREMENT,
-    authorId
-    INT
-    NOT
-    NULL,
-    taskId
-    INT
-    NOT
-    NULL,
-    date
-    DATETIME
-    NOT
-    NULL,
-    content
-    VARCHAR
-(
-    500
-) NOT NULL,
-    PRIMARY KEY
-(
-    id
-),
-    FOREIGN KEY
-(
-    authorId
-) REFERENCES users
-(
-    id
-),
-    FOREIGN KEY
-(
-    taskId
-) REFERENCES tasks
-(
-    id
-)
+    id       INT NOT NULL auto_increment,
+    authorid INT NOT NULL,
+    taskid   INT NOT NULL,
+    date     DATETIME NOT NULL,
+    content  VARCHAR (500) NOT NULL,
+    PRIMARY KEY ( id ),
+    FOREIGN KEY ( authorid ) REFERENCES users ( id ),
+    FOREIGN KEY ( taskid ) REFERENCES tasks ( id )
     );
